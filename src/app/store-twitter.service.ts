29ce835daf00f0742dd22017ef84aff0a198ef7c
saqhan <saqhan1@mail.ru>
2020-05-06 19:54:20 +0300

    https

--- src/app/store-twitter.service.ts
+++ src/app/store-twitter.service.ts
@@ -8,11 +8,11 @@ export class StoreTwitterService {
   constructor() { }
 
   messages = [
-    {id: 1, mess: 'Сегодня прекрасный день чтобы нарушуить режим карантина и всех заразить!', img: 'http://intocode.ru/d/react-chat/avatars/admin.jpg', favorite: false},
-    {id: 2, mess: 'Отличная идея! Я за!', img: 'http://intocode.ru/d/react-chat/avatars/adam.jpg', favorite: false},
-    {id: 3, mess: 'Где актив? ', img: 'http://intocode.ru/d/react-chat/avatars/admin.jpg', favorite: false},
-    {id: 4, mess: 'Ребята, давайте сделаем это! пусть нас и потом побреют налысо и ного переломают', img: 'http://intocode.ru/d/react-chat/avatars/no-avatar.jpg', favorite: false},
-    {id: 5, mess: 'Сегодня, в выходной день, после 19:00 самое то!', img: 'http://intocode.ru/d/react-chat/avatars/adam.jpg', favorite: false}
+    {id: 1, mess: 'Сегодня прекрасный день чтобы нарушуить режим карантина и всех заразить!', img: 'https://intocode.ru/d/react-chat/avatars/admin.jpg', favorite: false},
+    {id: 2, mess: 'Отличная идея! Я за!', img: 'https://intocode.ru/d/react-chat/avatars/adam.jpg', favorite: false},
+    {id: 3, mess: 'Где актив? ', img: 'https://intocode.ru/d/react-chat/avatars/admin.jpg', favorite: false},
+    {id: 4, mess: 'Ребята, давайте сделаем это! пусть нас и потом побреют налысо и ного переломают', img: 'https://intocode.ru/d/react-chat/avatars/no-avatar.jpg', favorite: false},
+    {id: 5, mess: 'Сегодня, в выходной день, после 19:00 самое то!', img: 'https://intocode.ru/d/react-chat/avatars/adam.jpg', favorite: false}
   ];
 
   onlyFavorite = false;
